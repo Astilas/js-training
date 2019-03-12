d5226740da2b5ce6326640681fe9cacc44a5dc15
Thibault Carayon <th.carayon@gmail.com>
2019-03-12 17:00:05 +0100

    multiply modified

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -10,9 +10,9 @@
 
 // Your code :
 function multiply(num1, num2){
-    if(num2>=0){
+    if(num2>0){
         return num1+ multiply(num1,num2-1);
-    } else if(num2<=0){
+    } else if(num2<0){
         return -multiply(num1, -num2);
     }else if(num2 ===0){
         return 0;
