34583e622eed67651ecc46a1dc639cc52c534422
Thibault Carayon <th.carayon@gmail.com>
2019-03-18 10:10:08 +0100

    exos js training TDD 4 done!

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,10 +10,23 @@
  *
  */
 
+ function jadenCase(string){
+     
+     string = string.split('');
+     for (let i=0; i < string.length; i++)
+        if (string[i] === " "){
+          string[i+1] = string[i+1].toUpperCase();
+        }
+    string[0] = string[0].toUpperCase();    
+    let resultat = string.join('');
+    return resultat;
+ }
 
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+/*assert.fail('You must write your own tests')*/
+assert.strictEqual(jadenCase('How are you'),'How Are You')
+assert.strictEqual(jadenCase('je code comme un wilder'),'Je Code Comme Un Wilder')
 // End of tests */
